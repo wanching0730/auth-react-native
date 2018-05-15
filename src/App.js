b3e25f445bda0a3f8cf726630d690ee988f82088
wanching_97 <wanching_97@hotmail.com>
2018-05-15 10:50:36 +0800

    test the shortened import statement

--- src/App.js
+++ src/App.js
@@ -1,10 +1,13 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+// destructure Header from index.js, default will enter index.js
+import { Header } from './components/common';
 
 class App extends Component {
     render() {
         return (
             <View>
+                <Header headerText="Authentication" />
                 <Text>An App</Text>
             </View>
         );
